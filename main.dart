8fcb2f1bff9a02b813d1d8b96dcec2bed134140c
HamadAlmatouq <Hamad.Almatouq@outlook.com>
2024-09-30 00:08:08 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+
+double tempInFaherenheit = 86.0;
+double celsius = 30.0;
+
+print(" 86F = ${(tempInFaherenheit - 32) / 1.8 }C");
+print(" 38C = ${celsius * 1.8000 + 32.00 }F");
+
+
+
+}
+
